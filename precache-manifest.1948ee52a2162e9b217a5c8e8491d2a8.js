1a59e205a4bce67baaa865fa9a088d3391f54b8d
Mohd Saquib <mohdsaquib0998@gmail.com>
2020-05-26 15:31:14 +0530

    Updates

---
+++ precache-manifest.1948ee52a2162e9b217a5c8e8491d2a8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6ee0c28ad6e5444f5c2cdc194ab1f59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12c4130cfd2237939623",
+    "url": "/static/css/main.634f6157.chunk.css"
+  },
+  {
+    "revision": "d83556264913e4757c6b",
+    "url": "/static/js/2.b3327a55.chunk.js"
+  },
+  {
+    "revision": "12c4130cfd2237939623",
+    "url": "/static/js/main.1d11695d.chunk.js"
+  },
+  {
+    "revision": "b44880e7568ba2754bcf",
+    "url": "/static/js/runtime-main.ad4cf4f0.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "0288c5ec7a87ea73f8d14b12fc4bc94e",
+    "url": "/static/media/talksCardBack.0288c5ec.svg"
+  }
+]);
